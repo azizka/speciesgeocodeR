2fa3707d1a85ef139ef40efd33152acaaca45ef1
Alexander Zizka <alexander.zizka@bioenv.gu.internal>
2016-11-03 16:09:13 +0100

    tidy repository

--- speciesgeocodeR/R/write.DESin.R
+++
@@ -1,6 +0,0 @@
-write.DESin <- function(x, file) {
-  for (i in 1:length(x[[3]])) {
-    write.table(x[[3]][[i]], paste(file, "_rep", i, ".txt", sep = ""), na = "NaN", 
-                sep = "\t", row.names = F, quote = F)
-  }
-}
--- vignettes/vignett_conversion into pdf.R
+++
@@ -1,2 +0,0 @@
-library(rmarkdown)
-render("C:/Users/xzizal/Dropbox (Antonelli Lab)/Arbeit/Gothenburg/Projects/3_speciesgeocoder/version104/vignettes/data_cleaning_and_exploration.Rmd", "pdf_document", clean = F)
